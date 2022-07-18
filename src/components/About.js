9086e98b5c682ee08062c8614a264968e5282423
Sean Oh <ohsean812@gmail.com>
2022-07-18 12:00:12 -0400

    Done with lab

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>Take a look at this cool GIF</p>
+    <img src={image} alt="I made this"></img>
+  </div>;
 }
 
 export default About;
